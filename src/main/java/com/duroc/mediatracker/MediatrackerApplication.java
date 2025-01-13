7e3a0e491d068752e5abbcc7d54dc81e821ad073
lexadecimals <alexis.wilkinsondefoe@gmail.com>
2025-01-13 10:13:55 +0000

    first commit :tada:

---
+++ src/main/java/com/duroc/mediatracker/MediatrackerApplication.java
@@ -0,0 +1,13 @@
+package com.duroc.mediatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediatrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediatrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duroc/mediatracker/MediatrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duroc.mediatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
