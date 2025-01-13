0c6e06080ec208bdd26f6923ebccd5dec5b594a1
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-13 16:43:34 +0000

    feat: add controller layer classes

---
+++ src/main/java/com/duroc/mediatracker/controller/FilmController.java
@@ -0,0 +1,16 @@
+package com.duroc.mediatracker.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mediatracker/films")
+public class FilmController {
+
+    @Autowired
+    FilmService filmService;
+
+
+}
---
+++ src/main/java/com/duroc/mediatracker/controller/ShowController.java
@@ -0,0 +1,9 @@
+package com.duroc.mediatracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/mediatracker/shows")
+public class ShowController {
+}
