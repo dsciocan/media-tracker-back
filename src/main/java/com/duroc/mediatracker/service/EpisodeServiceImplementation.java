4e5944dbe01d641f16bca52c048c879023e1fcdb
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-14 16:50:59 +0000

    chore: add implementation statement to episodeServiceImplementation class

--- src/main/java/com/duroc/mediatracker/service/EpisodeServiceImplementation.java
+++ src/main/java/com/duroc/mediatracker/service/EpisodeServiceImplementation.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EpisodeServiceImplementation {
+public class EpisodeServiceImplementation implements EpisodeService {
     @Autowired
     EpisodeRepository episodeRepository;
 }
