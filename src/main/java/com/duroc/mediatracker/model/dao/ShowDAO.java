435ab19ec0cda5390e0a56bc4dc488b12fc65cba
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-14 16:12:02 +0000

    feat: add requestShowDetails method to perform external api request for show details

--- src/main/java/com/duroc/mediatracker/model/dao/ShowDAO.java
+++ src/main/java/com/duroc/mediatracker/model/dao/ShowDAO.java
@@ -1,6 +1,7 @@
 package com.duroc.mediatracker.model.dao;
 
 import com.duroc.mediatracker.ExternalApiConfig;
+import com.duroc.mediatracker.model.show_detail.ShowDetails;
 import com.duroc.mediatracker.model.show_search.ShowSearchResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -12,9 +13,10 @@ import java.net.http.HttpResponse;
 
 public class ShowDAO {
 
-    public static ShowSearchResult requestData(String query) throws IOException, InterruptedException {
+    public static ExternalApiConfig apiKey = new ExternalApiConfig();
+
+    public static ShowSearchResult requestShowSearchData(String query) throws IOException, InterruptedException {
         String url = String.format("https://api.themoviedb.org/3/search/tv?query=%s&include_adult=false&language=en-US&page=1", query.replace(" ", "%20"));
-        ExternalApiConfig apiKey = new ExternalApiConfig();
         ShowSearchResult results;
         ObjectMapper mapper = new ObjectMapper();
         HttpRequest request = HttpRequest.newBuilder()
@@ -25,6 +27,21 @@ public class ShowDAO {
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         results = mapper.readValue(response.body(), ShowSearchResult.class);
+        return results;
+    }
+
+    public static ShowDetails requestShowDetails(long id) throws IOException, InterruptedException {
+        String url = String.format("https://api.themoviedb.org/3/tv/%s?language=en-US", id);
+        ShowDetails results;
+        ObjectMapper mapper = new ObjectMapper();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("accept", "application/json")
+                .header("Authorization", String.format("Bearer %s", apiKey.getApiKey()))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        results = mapper.readValue(response.body(), ShowDetails.class);
         return results;
     }
 }
