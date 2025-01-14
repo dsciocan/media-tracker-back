5973fa8a8f9febf709be6eb41417080e3e99de1f
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-14 17:04:58 +0000

    feat: implement getShowSearchResults method

--- src/main/java/com/duroc/mediatracker/service/ShowService.java
+++ src/main/java/com/duroc/mediatracker/service/ShowService.java
@@ -1,4 +1,9 @@
 package com.duroc.mediatracker.service;
 
+import com.duroc.mediatracker.model.show_search.ShowSearchResult;
+
+import java.io.IOException;
+
 public interface ShowService {
+    ShowSearchResult getShowSearchResults(String query) throws IOException, InterruptedException;
 }
