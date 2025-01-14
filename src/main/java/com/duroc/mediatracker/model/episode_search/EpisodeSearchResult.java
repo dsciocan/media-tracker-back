b05c93ca731193b324d04421d603d2f43df25823
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-14 16:11:12 +0000

    chore: add jsonignoreproperties annotation to episode data objects

--- src/main/java/com/duroc/mediatracker/model/episode_search/EpisodeSearchResult.java
+++ src/main/java/com/duroc/mediatracker/model/episode_search/EpisodeSearchResult.java
@@ -1,6 +1,9 @@
 package com.duroc.mediatracker.model.episode_search;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
-public record EpisodeSearchResult(List<Result> episodeSearchResults) {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record EpisodeSearchResult(List<Result> episodes) {
 }
--- src/main/java/com/duroc/mediatracker/model/episode_search/Result.java
+++ src/main/java/com/duroc/mediatracker/model/episode_search/Result.java
@@ -1,4 +1,7 @@
 package com.duroc.mediatracker.model.episode_search;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record Result(String air_date, int episode_number, String name, String overview, int runtime, int season_number) {
 }
