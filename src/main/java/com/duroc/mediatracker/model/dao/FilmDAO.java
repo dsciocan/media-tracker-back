7be619463d85835163de5925f4756d24b37c249d
Sean Winston <seanwinston@gmail.com>
2025-01-14 16:01:03 +0000

    feat: implement FilmDAO method to reuest for film data based on title query

--- src/main/java/com/duroc/mediatracker/model/dao/FilmDAO.java
+++ src/main/java/com/duroc/mediatracker/model/dao/FilmDAO.java
@@ -1,4 +1,29 @@
 package com.duroc.mediatracker.model.dao;
 
+import com.duroc.mediatracker.ExternalApiConfig;
+import com.duroc.mediatracker.model.film_search.FilmSearchResults;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class FilmDAO {
+    public static FilmSearchResults requestData(String query) throws IOException, InterruptedException {
+        String url = String.format("https://api.themoviedb.org/3/search/movie?query=%s&include_adult=false&language=en-US&page=1", query.replace(" ", "%20"));
+        ExternalApiConfig apiKey = new ExternalApiConfig();
+        FilmSearchResults results;
+        ObjectMapper mapper = new ObjectMapper();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("accept", "application/json")
+                .header("Authorization", String.format("Bearer %s", apiKey.getApiKey()))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        results = mapper.readValue(response.body(), FilmSearchResults.class);
+        return results;
+}
 }
