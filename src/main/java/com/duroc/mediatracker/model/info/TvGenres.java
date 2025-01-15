8c80a3831dbba114eb84029ec5eca6ac6251d20f
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-15 16:37:01 +0000

    refactor: remove unnecessary changes to tvgenres

---
+++ src/main/java/com/duroc/mediatracker/model/info/TvGenres.java
@@ -0,0 +1,20 @@
+package com.duroc.mediatracker.model.info;
+
+public enum TvGenres {
+    Action_Adventure,
+    Animation,
+    Comedy,
+    Crime,
+    Documentary,
+    Drama,
+    Family,
+    Kids,
+    Mystery,
+    News,
+    Reality,
+    SciFi_Fantasy,
+    Soap,
+    Talk,
+    War_Politics,
+    Western
+}
