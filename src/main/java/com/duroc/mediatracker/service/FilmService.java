f4a5d125df5f3bac7f5fcda4bbdc4a846528b63c
Sean Winston <seanwinston@gmail.com>
2025-01-15 14:17:49 +0000

    feat: create service method to get film search result and append image base url to the poster path

--- src/main/java/com/duroc/mediatracker/service/FilmService.java
+++ src/main/java/com/duroc/mediatracker/service/FilmService.java
@@ -1,4 +1,9 @@
 package com.duroc.mediatracker.service;
 
+import com.duroc.mediatracker.model.film_search.FilmSearchResults;
+
+import java.io.IOException;
+
 public interface FilmService {
+    FilmSearchResults getFilmSearchResults(String query) throws IOException, InterruptedException;
 }
--- src/main/java/com/duroc/mediatracker/service/FilmServiceImplementation.java
+++ src/main/java/com/duroc/mediatracker/service/FilmServiceImplementation.java
@@ -1,12 +1,37 @@
 package com.duroc.mediatracker.service;
 
+import com.duroc.mediatracker.model.dao.FilmDAO;
+import com.duroc.mediatracker.model.film_search.FilmSearchResults;
+import com.duroc.mediatracker.model.film_search.Result;
 import com.duroc.mediatracker.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.List;
+
 @Service
-public class FilmServiceImplementation {
+public class FilmServiceImplementation implements FilmService{
+
+    private static final String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
     @Autowired
     FilmRepository filmRepository;
+
+    @Autowired
+    FilmDAO filmDAO;
+
+    @Override
+    public FilmSearchResults getFilmSearchResults(String query) throws IOException, InterruptedException {
+        FilmSearchResults searchResults = filmDAO.requestData(query);
+
+        List<Result> updatedResults = searchResults.results().stream()
+                .map(result -> new Result(result.id(), result.overview(),
+                        BASE_IMAGE_URL + result.poster_path(),
+                        result.release_date(), result.title()
+                ))
+                .toList();
+
+        return new FilmSearchResults(updatedResults, searchResults.total_results());
+    }
 }
