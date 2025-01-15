c1d690d082d05d461ff32ac8b34f4d93fece8839
Sean Winston <seanwinston@gmail.com>
2025-01-15 14:15:41 +0000

    feat: make FimlDAO methods non static

--- src/main/java/com/duroc/mediatracker/model/dao/FilmDAO.java
+++ src/main/java/com/duroc/mediatracker/model/dao/FilmDAO.java
@@ -12,7 +12,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class FilmDAO {
-    public static FilmSearchResults requestData(String query) throws IOException, InterruptedException {
+    public FilmSearchResults requestData(String query) throws IOException, InterruptedException {
         String url = String.format("https://api.themoviedb.org/3/search/movie?query=%s&include_adult=false&language=en-US&page=1", query.replace(" ", "%20"));
         ExternalApiConfig apiKey = new ExternalApiConfig();
         FilmSearchResults results;
@@ -28,7 +28,7 @@ public class FilmDAO {
         return results;
     }
 
-    public static FilmDetails filmSearchDetails(Long movieID) throws IOException, InterruptedException {
+    public FilmDetails filmSearchDetails(Long movieID) throws IOException, InterruptedException {
         String url = String.format("https://api.themoviedb.org/3/movie/%d?language=en-US", movieID);
         ExternalApiConfig apiKey = new ExternalApiConfig();
         FilmDetails results;
