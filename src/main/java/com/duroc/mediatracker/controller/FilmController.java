0c6edcff5b5d88e4dd5622e55cf6448b7fddc3b7
Sean Winston <seanwinston@gmail.com>
2025-01-15 14:19:54 +0000

    feat: create controller method to get film search results

--- src/main/java/com/duroc/mediatracker/controller/FilmController.java
+++ src/main/java/com/duroc/mediatracker/controller/FilmController.java
@@ -1,10 +1,17 @@
 package com.duroc.mediatracker.controller;
 
 
+import com.duroc.mediatracker.model.film_search.FilmSearchResults;
 import com.duroc.mediatracker.service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/mediatracker/films")
@@ -12,6 +19,12 @@ public class FilmController {
 
     @Autowired
     FilmService filmService;
+
+    @GetMapping("/search/{query}")
+    public ResponseEntity<FilmSearchResults> getFilmSearchResults(@PathVariable String query) throws IOException, InterruptedException {
+        FilmSearchResults filmSearchResults = filmService.getFilmSearchResults(query);
+        return new ResponseEntity<>(filmSearchResults, HttpStatus.OK);
+    }
 
 
 }
