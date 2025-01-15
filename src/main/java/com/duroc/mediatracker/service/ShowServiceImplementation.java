38ea549485c618f46f4bb515aff6faab63215cbb
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-15 16:30:32 +0000

    feat: create getShowDetails and saveShowDetails methods

--- src/main/java/com/duroc/mediatracker/service/ShowServiceImplementation.java
+++ src/main/java/com/duroc/mediatracker/service/ShowServiceImplementation.java
@@ -1,6 +1,11 @@
 package com.duroc.mediatracker.service;
 
 import com.duroc.mediatracker.model.dao.ShowDAO;
+import com.duroc.mediatracker.model.info.Episode;
+import com.duroc.mediatracker.model.info.Show;
+import com.duroc.mediatracker.model.info.TvGenres;
+import com.duroc.mediatracker.model.show_detail.Genre;
+import com.duroc.mediatracker.model.show_detail.ShowDetails;
 import com.duroc.mediatracker.model.show_search.Result;
 import com.duroc.mediatracker.model.show_search.ShowSearchResult;
 import com.duroc.mediatracker.repository.ShowRepository;
@@ -8,12 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ShowServiceImplementation implements ShowService {
 
     @Autowired
     ShowRepository showRepository;
+
+    @Autowired
+    EpisodeService episodeService;
 
 
     @Override
@@ -24,5 +35,74 @@ public class ShowServiceImplementation implements ShowService {
             result.setPoster_path(url);
         }
         return showSearchResult;
+    }
+
+    @Override
+    public ShowDetails getShowDetails(Long id) throws IOException, InterruptedException {
+       ShowDetails showDetails =  ShowDAO.requestShowDetails(id);
+       String url = "https://image.tmdb.org/t/p/original" + showDetails.getPoster_path();
+       showDetails.setPoster_path(url);
+       return showDetails;
+    }
+
+//    @Override
+//    public Show getShowDetails2(Long id) throws IOException, InterruptedException {
+//        ShowDetails showDetails =  ShowDAO.requestShowDetails(id);
+//        String url = "https://image.tmdb.org/t/p/original" + showDetails.getPoster_path();
+//        showDetails.setPoster_path(url);
+//        List<TvGenres> showGenres = new ArrayList<>();
+//        for(Genre genre : showDetails.getGenres()) {
+//            for(TvGenres tvGenre : TvGenres.values()) {
+//                if(Objects.equals(tvGenre.getName(), genre.name())) {
+//                    showGenres.add(tvGenre);
+//                }
+//
+//            }
+//        }
+//        Show savedShow = Show.builder()
+//                .title(showDetails.getName())
+//                .synopsis(showDetails.getOverview())
+//                .releaseYear(Integer.parseInt(showDetails.getFirst_air_date().substring(0,4)))
+//                .finishedYear(Integer.parseInt(showDetails.getLast_air_date().substring(0,4)))
+//                .isComplete(!showDetails.isIn_production())
+//                .posterUrl(showDetails.getPoster_path())
+//                .genres(showGenres)
+//                .numberOfSeasons(showDetails.getNumber_of_seasons())
+//                .numberOfEpisodes(showDetails.getNumber_of_episodes())
+//                .country(showDetails.getOrigin_country().getFirst())
+//                .language(showDetails.getOriginal_language())
+//                .build();
+//        savedShow.setEpisodes(episodeService.getAllEpisodes2(showDetails.getId(), savedShow.getNumberOfSeasons()));
+//        return savedShow;
+//    }
+
+    @Override
+    public Show saveShowDetails(Long apiId) throws IOException, InterruptedException {
+        ShowDetails showDetails =  getShowDetails(apiId);
+        List<String> showGenres = new ArrayList<>();
+        for(Genre genre : showDetails.getGenres()) {
+            showGenres.add(genre.name());
+        }
+
+        Show savedShow = Show.builder()
+                .title(showDetails.getName())
+                .synopsis(showDetails.getOverview())
+                .releaseYear(Integer.parseInt(showDetails.getFirst_air_date().substring(0,4)))
+                .finishedYear(Integer.parseInt(showDetails.getLast_air_date().substring(0,4)))
+                .isComplete(!showDetails.isIn_production())
+                .posterUrl(showDetails.getPoster_path())
+                .genres(showGenres)
+                .numberOfSeasons(showDetails.getNumber_of_seasons())
+                .numberOfEpisodes(showDetails.getNumber_of_episodes())
+                .country(showDetails.getOrigin_country().getFirst())
+                .language(showDetails.getOriginal_language())
+                .build();
+        showRepository.save(savedShow);
+//        savedShow = showRepository.findById(savedShow.getId()).get();
+        episodeService.saveEpisodes(showDetails.getId(), savedShow.getNumberOfSeasons(), savedShow);
+        List<Episode> episodeList = episodeService.getSavedEpisodesByShowId(savedShow.getId());
+        savedShow.setEpisodes(episodeList);
+        showRepository.save(savedShow);
+        return savedShow;
     }
 }
