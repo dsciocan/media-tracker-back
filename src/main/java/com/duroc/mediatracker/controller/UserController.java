1fa82809d586f3b04ba8f4bbd184242c0a793a99
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-16 15:17:53 +0000

    feat: add user crud controller methods/endpoints

--- src/main/java/com/duroc/mediatracker/controller/UserController.java
+++ src/main/java/com/duroc/mediatracker/controller/UserController.java
@@ -1,9 +1,37 @@
 package com.duroc.mediatracker.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.duroc.mediatracker.model.user.AppUser;
+import com.duroc.mediatracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/mediatracker/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<AppUser> getUserById(@PathVariable Long userId) {
+        return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser appUser) {
+        return new ResponseEntity<>(userService.saveUser(appUser), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<AppUser> changeUsername(@PathVariable Long userId, @RequestBody String newUsername) {
+        return new ResponseEntity<>(userService.changeUsername(userId, newUsername), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
+        return new ResponseEntity<>(userService.deleteUser(userId), HttpStatus.OK);
+    }
+
 }
