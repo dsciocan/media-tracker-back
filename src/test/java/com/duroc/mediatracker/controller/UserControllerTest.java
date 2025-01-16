d4e1fb50d4c396efb1030a3f550317cc46acbfb3
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-16 15:27:38 +0000

    chore: add UserControllerTest setup annotations

--- src/test/java/com/duroc/mediatracker/controller/UserControllerTest.java
+++ src/test/java/com/duroc/mediatracker/controller/UserControllerTest.java
@@ -1,13 +1,41 @@
 package com.duroc.mediatracker.controller;
 
+import com.duroc.mediatracker.service.ShowService;
+import com.duroc.mediatracker.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@AutoConfigureMockMvc
+@SpringBootTest
 class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(){
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
 
     @Test
     void getUserById() {
+
     }
 
     @Test
