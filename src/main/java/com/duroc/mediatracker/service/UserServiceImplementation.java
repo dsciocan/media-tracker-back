8dbd787a7d38c60d7633b158756a183257d23c6a
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-16 14:44:56 +0000

    feat: add basic CRUD User service layer methods (getUserById, saveUser, changeUsername, deleteUser)

--- src/main/java/com/duroc/mediatracker/service/UserServiceImplementation.java
+++ src/main/java/com/duroc/mediatracker/service/UserServiceImplementation.java
@@ -1,5 +1,8 @@
 package com.duroc.mediatracker.service;
 
+import com.duroc.mediatracker.Exception.InvalidItemException;
+import com.duroc.mediatracker.Exception.ItemNotFoundException;
+import com.duroc.mediatracker.model.user.AppUser;
 import com.duroc.mediatracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,42 @@ public class UserServiceImplementation implements UserService{
 
     @Autowired
     UserRepository userRepository;
+
+    @Override
+    public AppUser getUserById(Long id) {
+        if(userRepository.findById(id).isPresent()) {
+            return userRepository.findById(id).get();
+        } else {
+            throw new ItemNotFoundException(String.format("User with id %s not found", id));
+        }
+    }
+
+    @Override
+    public AppUser saveUser(AppUser appUser) {
+        if(appUser.getUsername().isEmpty()) {
+            throw new InvalidItemException("Username cannot be empty");
+        } else {
+            return userRepository.save(appUser);
+        }
+    }
+
+    @Override
+    public AppUser changeUsername(Long userId, String newUsername) {
+        AppUser user = getUserById(userId);
+        if(newUsername.isEmpty()) {
+            throw new InvalidItemException("Username cannot be empty");
+        } else {
+            user.setUsername(newUsername);
+            return userRepository.save(user);
+        }
+    }
+
+    @Override
+    public String deleteUser(Long userId) {
+        AppUser user = getUserById(userId);
+        userRepository.delete(user);
+        return "User deleted successfully";
+
+    }
+
 }
