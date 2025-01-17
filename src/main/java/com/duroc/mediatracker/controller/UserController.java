08cfe0d34530571680c9ca2ead76e6669c78cf9a
Shamar <Shamar098@gmail.com>
2025-01-17 10:42:25 +0000

    feat: add token verify test endpoint

--- src/main/java/com/duroc/mediatracker/controller/UserController.java
+++ src/main/java/com/duroc/mediatracker/controller/UserController.java
@@ -7,10 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @RestController
 @RequestMapping("/api/v1/mediatracker/users")
 public class UserController {
-
     @Autowired
     UserService userService;
 
@@ -33,5 +39,38 @@ public class UserController {
     public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
         return new ResponseEntity<>(userService.deleteUser(userId), HttpStatus.OK);
     }
+    /**
+     * todo:
+     *      Spring Security set up for the "user" endpoint
+     *      (Potentially) refactor Show and Film endpoint('s) for user specified things:
+     *         - move them to the User Controller
+     *      Filter for the endpoint that can be applied to Spring Security(?)
+     *      Specific endpoint for adding a User to the database (eg: they just signed up)
+     *          - + all logic that comes with that (Service, Repo, Tests etc..)
+     */
 
+
+    // This was just a test endpoint for receiving a token from the client and authenticating it
+    @PostMapping("/auth")
+    public ResponseEntity<String> authenticateUser(HttpServletRequest request, HttpServletResponse response) {
+        String idToken = request.getHeader("Authorization");
+
+        // idToken comes from the client app (shown above)
+        FirebaseToken decodedToken;
+
+        try {
+            decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+        } catch (FirebaseAuthException e) {
+            throw new RuntimeException(e);
+        }
+
+        // uid Can be the unique identifier of the user on the database (Appuser's OauthID field possibly (?))
+        String uid = decodedToken.getUid();
+
+        //Through the decodedToken -> a few credentials can be obtained eg: the user's name or email
+        System.out.println("User name: " + decodedToken.getName());
+
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
