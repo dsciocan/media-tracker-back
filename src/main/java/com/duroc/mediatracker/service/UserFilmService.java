2686553c27ccf76249b067f064065573217ce789
Sean Winston <seanwinston@gmail.com>
2025-01-17 15:05:12 +0000

    fix saveUserFilm method

--- src/main/java/com/duroc/mediatracker/service/UserFilmService.java
+++ src/main/java/com/duroc/mediatracker/service/UserFilmService.java
@@ -2,7 +2,9 @@ package com.duroc.mediatracker.service;
 
 import com.duroc.mediatracker.model.user.UserFilm;
 
+import java.io.IOException;
+
 public interface UserFilmService {
 
-    UserFilm saveUserFilm(UserFilm userFilm);
+    UserFilm saveUserFilm(UserFilm userFilm, Long userId, Long movieId) throws IOException, InterruptedException;
 }
--- src/main/java/com/duroc/mediatracker/service/UserFilmServiceImplementation.java
+++ src/main/java/com/duroc/mediatracker/service/UserFilmServiceImplementation.java
@@ -1,12 +1,14 @@
 package com.duroc.mediatracker.service;
 
-import com.duroc.mediatracker.Exception.InvalidItemException;
+import com.duroc.mediatracker.model.info.Film;
+import com.duroc.mediatracker.model.user.AppUser;
 import com.duroc.mediatracker.model.user.UserFilm;
-import com.duroc.mediatracker.repository.FilmRepository;
+import com.duroc.mediatracker.model.user.UserFilmId;
 import com.duroc.mediatracker.repository.UserFilmRepository;
-import com.duroc.mediatracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class UserFilmServiceImplementation implements UserFilmService{
@@ -15,21 +17,22 @@ public class UserFilmServiceImplementation implements UserFilmService{
     UserFilmRepository userFilmRepository;
 
     @Autowired
-    FilmRepository filmRepository;
+    FilmService filmService;
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @Override
-    public UserFilm saveUserFilm(UserFilm userFilm) {
-        Long filmId = userFilm.getUserFilmId().getFilm().getId();
-        Long userId = userFilm.getUserFilmId().getAppUser().getId();
-        if(!filmRepository.existsById(filmId)){
-            throw new InvalidItemException("Invalid film ID: " + filmId);
-        }
-        if(!userRepository.existsById(userId)){
-            throw new InvalidItemException("Invalid user ID: " + userId);
-        }
+    public UserFilm saveUserFilm(UserFilm userFilm, Long userId, Long movieId) throws IOException, InterruptedException {
+        AppUser appUser = userService.getUserById(userId);
+        Film film = filmService.addFilmToList(movieId);
+
+        //set the composite key
+        UserFilmId userFilmId = new UserFilmId();
+        userFilmId.setAppUser(appUser);
+        userFilmId.setFilm(film);
+        userFilm.setUserFilmId(userFilmId);
+
         return userFilmRepository.save(userFilm);
     }
 }
