e17b16801370937be90aa894d676bad877690fee
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-17 13:48:46 +0000

    refactor: update response mapping to return a list of Result objects for requestShowSearchData

--- src/main/java/com/duroc/mediatracker/model/dao/ShowDAO.java
+++ src/main/java/com/duroc/mediatracker/model/dao/ShowDAO.java
@@ -2,7 +2,9 @@ package com.duroc.mediatracker.model.dao;
 
 import com.duroc.mediatracker.ExternalApiConfig;
 import com.duroc.mediatracker.model.show_detail.ShowDetails;
+import com.duroc.mediatracker.model.show_search.Result;
 import com.duroc.mediatracker.model.show_search.ShowSearchResult;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -10,14 +12,17 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
 
 public class ShowDAO {
 
     public static ExternalApiConfig apiKey = new ExternalApiConfig();
 
-    public static ShowSearchResult requestShowSearchData(String query) throws IOException, InterruptedException {
+    public static List<Result> requestShowSearchData(String query) throws IOException, InterruptedException {
         String url = String.format("https://api.themoviedb.org/3/search/tv?query=%s&include_adult=false&language=en-US&page=1", query.replace(" ", "%20"));
-        ShowSearchResult results;
+//        ShowSearchResult results;
+        Result[] results;
         ObjectMapper mapper = new ObjectMapper();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -26,8 +31,13 @@ public class ShowDAO {
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        results = mapper.readValue(response.body(), ShowSearchResult.class);
-        return results;
+
+        JsonNode root = mapper.readTree(response.body());
+        JsonNode resultsJson = root.get("results");
+
+        results = mapper.readValue(resultsJson.toString(), Result[].class);
+
+        return Arrays.asList(results);
     }
 
     public static ShowDetails requestShowDetails(long id) throws IOException, InterruptedException {
