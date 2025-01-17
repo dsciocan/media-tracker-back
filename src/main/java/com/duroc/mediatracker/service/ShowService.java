c4b8d0aa951877fa66cb0367dc31e183de06b07f
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-17 13:52:29 +0000

    refactor: update getShowSearchResults to return a list of Result objects

--- src/main/java/com/duroc/mediatracker/service/ShowService.java
+++ src/main/java/com/duroc/mediatracker/service/ShowService.java
@@ -2,16 +2,19 @@ package com.duroc.mediatracker.service;
 
 import com.duroc.mediatracker.model.info.Show;
 import com.duroc.mediatracker.model.show_detail.ShowDetails;
-import com.duroc.mediatracker.model.show_search.ShowSearchResult;
+import com.duroc.mediatracker.model.show_search.Result;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface ShowService {
-    ShowSearchResult getShowSearchResults(String query) throws IOException, InterruptedException;
+    List<Result> getShowSearchResults(String query) throws IOException, InterruptedException;
     ShowDetails getShowDetails(Long apiId) throws IOException, InterruptedException;
 
 
 //    Show getShowDetails2(Long id) throws IOException, InterruptedException;
+
+//    Show saveShowAsUser(Show show);
 
     Show saveShowDetails(Long apiId) throws IOException, InterruptedException;
     Show getSavedShow(Long id);
