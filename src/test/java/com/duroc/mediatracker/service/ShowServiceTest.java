f0b69dcb9632e2e51d3ba941912a60a42d96c446
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-17 13:54:08 +0000

    refactor: update ShowService/Controller tests to reflect search return type change

--- src/test/java/com/duroc/mediatracker/service/ShowServiceTest.java
+++ src/test/java/com/duroc/mediatracker/service/ShowServiceTest.java
@@ -1,11 +1,9 @@
 package com.duroc.mediatracker.service;
 
-import com.duroc.mediatracker.model.dao.ShowDAO;
 import com.duroc.mediatracker.model.info.Episode;
 import com.duroc.mediatracker.model.info.Show;
 import com.duroc.mediatracker.model.show_detail.ShowDetails;
 import com.duroc.mediatracker.model.show_search.Result;
-import com.duroc.mediatracker.model.show_search.ShowSearchResult;
 import com.duroc.mediatracker.repository.ShowRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -42,10 +40,10 @@ class ShowServiceTest {
         Result res2 = new Result(206584, "The Game of Thrones Reunion Hosted by Conan O'Brien", "A two-part reunion show with cast members from the final season including Kit Harington, Emilia Clarke, Sophie Turner and more, as well as previously departed fan-favorites like Sean Bean, Jason Momoa, Mark Addy and others.", "https://image.tmdb.org/t/p/original/xwq2TALKkuCujBnUTpr57wDO61y.jpg", "2021-04-05");
         Result res3 = new Result(94997, "House of the Dragon", "The Targaryen dynasty is at the absolute apex of its power, with more than 15 dragons under their yoke. Most empires crumble from such heights. In the case of the Targaryens, their slow fall begins when King Viserys breaks with a century of tradition by naming his daughter Rhaenyra heir to the Iron Throne. But when Viserys later fathers a son, the court is shocked when Rhaenyra retains her status as his heir, and seeds of division sow friction across the realm.", "https://image.tmdb.org/t/p/original/t9XkeE7HzOsdQcDDDapDYh8Rrmt.jpg", "2022-08-21");
         Result res4 = new Result(224372, "A Knight of the Seven Kingdoms: The Hedge Knight", "Set in an age when the Targaryen line still holds the Iron Throne and the memory of the last dragon has not yet passed from living memory, great destinies, powerful foes and dangerous exploits all await these improbable and incomparable friends.", "https://image.tmdb.org/t/p/original/7Pb4gk51oUKWarOhvaSgf5RekIj.jpg", "");
-        ShowSearchResult expectedResult = new ShowSearchResult(List.of(res1, res2, res3, res4), 0);
+        List<Result> expectedResult = List.of(res1, res2, res3, res4);
 
         assertAll(() -> {
-            assertEquals(expectedResult.results().size(), showServiceImplementation.getShowSearchResults("game of thrones").results().size());
+            assertEquals(expectedResult.size(), showServiceImplementation.getShowSearchResults("game of thrones").size());
             assertEquals(expectedResult, showServiceImplementation.getShowSearchResults("game of thrones"));
         });
     }
