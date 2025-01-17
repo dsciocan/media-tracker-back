0c598ec26c3f5d257626e2d1588effa66cff13de
Sean Winston <seanwinston@gmail.com>
2025-01-17 11:30:54 +0000

    feat: create UserFilmService method to save a UserFilm

--- src/main/java/com/duroc/mediatracker/service/UserFilmService.java
+++ src/main/java/com/duroc/mediatracker/service/UserFilmService.java
@@ -1,4 +1,8 @@
 package com.duroc.mediatracker.service;
 
+import com.duroc.mediatracker.model.user.UserFilm;
+
 public interface UserFilmService {
+
+    UserFilm saveUserFilm(UserFilm userFilm);
 }
--- src/main/java/com/duroc/mediatracker/service/UserFilmServiceImplementation.java
+++ src/main/java/com/duroc/mediatracker/service/UserFilmServiceImplementation.java
@@ -1,6 +1,10 @@
 package com.duroc.mediatracker.service;
 
+import com.duroc.mediatracker.Exception.InvalidItemException;
+import com.duroc.mediatracker.model.user.UserFilm;
+import com.duroc.mediatracker.repository.FilmRepository;
 import com.duroc.mediatracker.repository.UserFilmRepository;
+import com.duroc.mediatracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +13,23 @@ public class UserFilmServiceImplementation implements UserFilmService{
 
     @Autowired
     UserFilmRepository userFilmRepository;
+
+    @Autowired
+    FilmRepository filmRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserFilm saveUserFilm(UserFilm userFilm) {
+        Long filmId = userFilm.getUserFilmId().getFilm().getId();
+        Long userId = userFilm.getUserFilmId().getAppUser().getId();
+        if(!filmRepository.existsById(filmId)){
+            throw new InvalidItemException("Invalid film ID: " + filmId);
+        }
+        if(!userRepository.existsById(userId)){
+            throw new InvalidItemException("Invalid user ID: " + userId);
+        }
+        return userFilmRepository.save(userFilm);
+    }
 }
