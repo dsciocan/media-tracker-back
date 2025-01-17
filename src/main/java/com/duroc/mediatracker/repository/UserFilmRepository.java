691e0c026aeb52b6dbec2bf9e2be8013a402e7d4
Sean Winston <seanwinston@gmail.com>
2025-01-17 15:36:19 +0000

    feat: add findByUserFilmIdAppUser method to UserFilmRepository

--- src/main/java/com/duroc/mediatracker/repository/UserFilmRepository.java
+++ src/main/java/com/duroc/mediatracker/repository/UserFilmRepository.java
@@ -1,10 +1,14 @@
 package com.duroc.mediatracker.repository;
 
+import com.duroc.mediatracker.model.user.AppUser;
 import com.duroc.mediatracker.model.user.UserFilm;
 import com.duroc.mediatracker.model.user.UserFilmId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserFilmRepository extends JpaRepository<UserFilm, UserFilmId> {
+    List<UserFilm> findByUserFilmIdAppUser(AppUser user);
 }
