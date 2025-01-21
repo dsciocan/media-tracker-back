3d89166d49537f16ab09bb12bdb393be010e940c
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-21 09:44:32 +0000

    fix: security filter fix allowing post requests

--- src/main/java/com/duroc/mediatracker/config/SecurityConfig.java
+++ src/main/java/com/duroc/mediatracker/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -20,6 +21,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.securityMatcher("/api/v1/mediatracker/users/**")
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .addFilterAfter(new FirebaseBearerTokenFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(
                         authorizeRequests -> authorizeRequests
