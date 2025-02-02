831274cec75b3954e597177066e52a8ca9c98e22
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-02 11:41:07 +0000

    feat: add findFilmByTmdbId method

--- src/main/java/com/duroc/mediatracker/repository/FilmRepository.java
+++ src/main/java/com/duroc/mediatracker/repository/FilmRepository.java
@@ -4,6 +4,11 @@ import com.duroc.mediatracker.model.info.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FilmRepository extends JpaRepository<Film, Long> {
+
+    List<Film> findFilmByTmdbId(Long tbdmId);
+
 }
