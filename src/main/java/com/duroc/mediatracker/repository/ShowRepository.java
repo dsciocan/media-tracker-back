a02dbd232ad3ff8d3a99a489ddd7163acdcef292
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-05 18:27:44 +0000

    feat: add findShowByTmdbId repository method

--- src/main/java/com/duroc/mediatracker/repository/ShowRepository.java
+++ src/main/java/com/duroc/mediatracker/repository/ShowRepository.java
@@ -4,6 +4,10 @@ import com.duroc.mediatracker.model.info.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ShowRepository extends JpaRepository<Show, Long> {
+
+    public List<Show> findShowByTmdbId(Long tmdbId);
 }
