03e38b86a1ba684e070cd37c5404a7889e100da5
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-24 12:15:10 +0000

    refactor: modify save/update methods to account for watched show status; implement getMostRecentEpisode

--- src/main/java/com/duroc/mediatracker/service/UserEpisodeServiceImplementation.java
+++ src/main/java/com/duroc/mediatracker/service/UserEpisodeServiceImplementation.java
@@ -1,19 +1,17 @@
 package com.duroc.mediatracker.service;
 
-import com.duroc.mediatracker.Exception.InvalidItemException;
 import com.duroc.mediatracker.Exception.ItemNotFoundException;
 import com.duroc.mediatracker.model.info.Episode;
-import com.duroc.mediatracker.model.user.AppUser;
-import com.duroc.mediatracker.model.user.UserEpisode;
-import com.duroc.mediatracker.model.user.UserEpisodeId;
+import com.duroc.mediatracker.model.user.*;
 import com.duroc.mediatracker.repository.UserEpisodeRepository;
+import com.duroc.mediatracker.repository.UserShowRepository;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class UserEpisodeServiceImplementation implements UserEpisodeService {
@@ -25,24 +23,36 @@ public class UserEpisodeServiceImplementation implements UserEpisodeService {
     EpisodeService episodeService;
 
     @Autowired
+    UserShowRepository userShowRepository;
+
+    @Autowired
     UserService userService;
+
+    @Autowired
+    ShowService showService;
 
     @Override
     public List<UserEpisode> getUserEpisodeListByShowId(Long showId) {
-        AppUser user = userService.getUser();
-        List<UserEpisode> allUserEpisodes = userEpisodeRepository.findByUserEpisodeIdAppUser(user);
-        return allUserEpisodes.stream().filter(ep -> Objects.equals(ep.getUserEpisodeId().getEpisode().getShow().getId(), showId)).toList();
+
+        return userEpisodeRepository.findByShowId(showId);
+
     }
+
 
     @Override
     public List<UserEpisode> saveAllShowEpisodesAsUserEpisodes(Long showId) {
         AppUser user = userService.getUser();
         List<Episode> showEpisodes = episodeService.getSavedEpisodesByShowId(showId);
+        UserShowId userShowId = new UserShowId(user, showService.getSavedShow(showId));
+        UserShow show = userShowRepository.findById(userShowId).orElseThrow();
         List<UserEpisode> userEpisodeList = new ArrayList<>();
         showEpisodes.forEach((ep) ->
         {
             UserEpisodeId userEpisodeId = new UserEpisodeId(user, ep);
-            UserEpisode userEpisode = new UserEpisode(userEpisodeId, 0, "", false, null);
+            UserEpisode userEpisode = new UserEpisode(userEpisodeId, 0, "", false, null, showService.getSavedShow(showId));
+            if(show.getStatus().equalsIgnoreCase("WATCHED")) {
+                userEpisode.setWatched(true);
+            }
             userEpisodeList.add(userEpisode);
             userEpisodeRepository.save(userEpisode);
         });
@@ -63,13 +73,13 @@ public class UserEpisodeServiceImplementation implements UserEpisodeService {
 
     @Override
     public UserEpisode changeUserEpisodeDetails(Long episodeId, UserEpisode newUserEpisode) {
-        UserEpisode userEpisode = getUserEpisodeByEpisodeId(episodeId);
+        AppUser user = userService.getUser();
+        Episode episode = episodeService.getSavedEpisodeById(episodeId);
+        UserEpisodeId userEpisodeId = new UserEpisodeId(user, episode);
+        UserEpisode userEpisode = userEpisodeRepository.findById(userEpisodeId).orElseThrow();
         if(newUserEpisode.getUserEpisodeId() == null) {
             newUserEpisode.setUserEpisodeId(userEpisode.getUserEpisodeId());
         }
-        if(!Objects.equals(newUserEpisode.getUserEpisodeId(), userEpisode.getUserEpisodeId())) {
-            throw new InvalidItemException("User and Episode properties are not changeable");
-        } else {
             userEpisode.setNotes(newUserEpisode.getNotes());
             if(newUserEpisode.isWatched() && !userEpisode.isWatched()) {
                 userEpisode.setWatchedDate(LocalDate.now());
@@ -77,9 +87,23 @@ public class UserEpisodeServiceImplementation implements UserEpisodeService {
             userEpisode.setWatched(newUserEpisode.isWatched());
             userEpisode.setRating(newUserEpisode.getRating());
             return userEpisodeRepository.save(userEpisode);
-        }
     }
 
+
+    @Override
+    public UserEpisode getMostRecentEpisode(Long showId) {
+        List<UserEpisode> userEpisodeList = getUserEpisodeListByShowId(showId);
+        if(userEpisodeList.getLast().isWatched()) {
+            return userEpisodeList.getLast();
+        } else {
+            for (int i = userEpisodeList.size() - 1; i >= 0; i--) {
+                if (userEpisodeList.get(i).isWatched()) {
+                    return userEpisodeList.get(i + 1);
+                }
+            }
+        }
+        return userEpisodeList.getFirst();
+    }
 
     @Override
     public int getAllRuntimeWatched() {
